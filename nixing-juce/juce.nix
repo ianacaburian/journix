7a2e52861e0cfbf46a81eb1e9039939a747cafbf
Ian Caburian <ianacaburian@gmail.com>
2023-08-27 04:08:31 +1000

    Added nixing-juce project.

--- nixing-juce/juce.nix
+++ nixing-juce/juce.nix
@@ -2,32 +2,32 @@ let
 pkgs = import <nixpkgs> {};
 in
 pkgs.darwin.apple_sdk_11_0.stdenv.mkDerivation {
-  name = "juce";
- 
+  pname = "juce";
+  version = "7.0.7";
   src = pkgs.fetchFromGitHub {
     owner = "juce-framework";
     repo = "juce";
     rev = "7.0.7";
-    sha256 = "fsYoFYRhJZlrTw5VYA1ZPEXTTseWIVfhZjQUxVwwYTk=";
-    deepClone = true;
+    sha256 = "r+Wf/skPDexm3rsrVBoWrygKvV9HGlCQd7r0iHr9avM=";
   };
 
-  nativeBuildInputs = with pkgs; [ cmake ];
+  nativeBuildInputs = [ pkgs.cmake ];
 
-  buildInputs = with pkgs.darwin.apple_sdk_11_0.frameworks; [ 
-    Cocoa
+  buildInputs = with pkgs.darwin.apple_sdk_11_0.frameworks; [
+    Cocoa 
     MetalKit
     WebKit
   ];
 
   configurePhase = ''
     runHook preConfigure
-    cmake -B juce-nix-build -DCMAKE_INSTALL_PREFIX=$out
+    cmake -B cmake-build-install -DCMAKE_INSTALL_PREFIX=$out
     runHook postConfigure
   '';
+
   buildPhase = ''
     runHook preBuild
-    cmake --build juce-nix-build --target install
+    cmake --build cmake-build-install --target install
     runHook postBuild
   '';
 }
