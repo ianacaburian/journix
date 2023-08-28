017dc917d7797642a8fdc125bb64870a83fb57e8
Ian Caburian <ianacaburian@gmail.com>
2023-08-29 03:30:11 +1000

    Finished juce flakes.

--- juce-flakes/flake.nix
+++ juce-flakes/flake.nix
@@ -2,57 +2,43 @@
   description = "juce flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-    utils.url = "github:numtide/flake-utils";
-
-    juce = {
-      type = "github";
-      owner = "juce-framework";
-      repo = "JUCE";
-      # rev = "7.0.7";
-      # sha256 = "r+Wf/skPDexm3rsrVBoWrygKvV9HGlCQd7r0iHr9avM=";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    juce-src = {
+      url = "github:juce-framework/JUCE/7.0.7";
       flake = false;
-      # rev = "6d7bf9281a2e500e734fe46840ab7a2b1cbde544";
-      # sha256 = "LJRPKNGKn5oS3HWvs4m05ktdta58po6WJA+YRf94Ftw=";
     };
   };
 
-  outputs = { self, nixpkgs, juce, ... }@inputs: 
-    inputs.utils.lib.eachSystem [
-      "x86_64-darwin"
-    ] (system: 
-      let 
-      pkgs = import nixpkgs { inherit system; };
-      in 
-      {
-        packages.system.juce = pkgs.stdenv.mkDerivation {
-          name = "juce";
-          src = juce;
-          nativeBuildInputs = [ pkgs.cmake ];
-          buildInputs = with pkgs.darwin.apple_sdk_11_0.frameworks; [
-            Cocoa 
-            MetalKit
-            WebKit
-          ];
-          configurePhase = ''
-            runHook preConfigure
-            cmake -B cmake-build-install -DCMAKE_INSTALL_PREFIX=$out
-            runHook postConfigure
-          '';
-          buildPhase = ''
-            runHook preBuild
-            cmake --build cmake-build-install --target install
-            runHook postBuild
-          '';
-        };
-
-        devShell = pkgs.mkShell {
-          name = "pg shell";
-          packages = with pkgs; [
-            cmake 
-            self.packages.system.juce
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs, juce-src }: 
+    let 
+    pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+    juce = pkgs.darwin.apple_sdk_11_0.stdenv.mkDerivation {
+      name = "juce";
+      src = juce-src;
+      nativeBuildInputs = [ pkgs.cmake ];
+      buildInputs = with pkgs.darwin.apple_sdk_11_0.frameworks; [
+        Cocoa 
+        MetalKit
+        WebKit
+      ];
+      configurePhase = ''
+        runHook preConfigure
+        cmake -B cmake-build-install -DCMAKE_INSTALL_PREFIX=$out
+        runHook postConfigure
+      '';
+      buildPhase = ''
+        runHook preBuild
+        cmake --build cmake-build-install --target install
+        runHook postBuild
+      '';
+    };
+    in {
+      devShell.x86_64-darwin = pkgs.mkShell { 
+        name = "juce-flakes shell";
+        buildInputs = [
+          pkgs.cmake
+          juce
+        ];
+      };
+    };
 }
